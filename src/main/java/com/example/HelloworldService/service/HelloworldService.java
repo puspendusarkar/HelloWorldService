88bf6b92b02fd4aecbb8a33aa5fc0a7d57f61c46
pusarkar <puspendusarkar@gmail.com>
2022-01-14 16:46:42 -0500

    changes on service class

--- src/main/java/com/example/HelloworldService/service/HelloworldService.java
+++ src/main/java/com/example/HelloworldService/service/HelloworldService.java
@@ -13,10 +13,12 @@ public class HelloworldService {
         Person person1=new Person(1,"Susanta");
         Person person2=new Person(2,"Sorav");
         Person person3=new Person(3,"Sumit");
+        Person person4=new Person(4,"Amit");
         List<Person>  persons=new ArrayList<Person>();
         persons.add(person1);
         persons.add(person2);
         persons.add(person3);
+        persons.add(person4);
         return persons;
     }
 
